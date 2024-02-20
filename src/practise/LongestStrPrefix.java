2480d0b3315b6bddbdc7fe0e8d2f1046f98c794f
Sampath Dhand <sampathdhand@Sampaths-Air.lan>
2024-02-20 17:26:27 -0600

    apple interview

---
+++ src/practise/LongestStrPrefix.java
@@ -0,0 +1,27 @@
+package practise;
+
+import java.util.List;
+
+public class LongestStrPrefix {
+
+    public static void main(String[] args) {
+        String art[] = {"apple", "ape", "apricot"};
+        System.out.println(commonPrefix(art));
+    }
+
+
+    private static String commonPrefix(String[] strList) {
+        if (strList.length == 0)
+            return "";
+        String prefix = strList[0];
+        for (int i = 0; i < strList.length; i++) {
+            while (strList[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
+}
+
+
